459c15914e76a2c0a8e1b4f6e6e1171a0fc360bd
cornmeal <1152345818@qq.com>
2018-07-02 09:06:18 +0800

    增加DB.properties

---
+++ O-MATCHA/src/com/omatcha/test/Test.java
@@ -0,0 +1,16 @@
+package com.omatcha.test;
+
+import static org.junit.Assert.*;
+/***
+ * omatcha测试类
+ * @author Administrator
+ *
+ */
+public class Test {
+
+	@org.junit.Test
+	public void test() {
+
+	}
+
+}
